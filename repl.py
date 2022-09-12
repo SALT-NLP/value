3a24d642afe23e145085059f86c66d9eafee5e76
Helw150 <wbh230@nyu.edu>
2022-09-12 15:34:05 -0400

    Fix Segmentation Fault and Add Repl

---
+++ repl.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import pickle as pkl
+
+import argparse
+from src.Dialects import AfricanAmericanVernacular
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A REPL for the VALUE Transformations")
+    parser.add_argument("--transform", type=str, default=None, required=False)
+    args = parser.parse_args()
+
+    if args.transform == "aave_like":
+        mapping = {}
+        if os.path.exists("./resources/sae_aave_mapping_dict.pkl"):
+            with open("./resources/sae_aave_mapping_dict.pkl", "rb") as infile:
+                mapping = pkl.load(infile)
+        converter = AfricanAmericanVernacular(mapping, morphosyntax=True)
+    else:
+        print("Please select a dialect with a supported set of transformations!")
+        sys.exit()
+
+    while True:
+        example = input("Enter an input in SAE: ")
+        print(
+            "After "
+            + converter.dialect_name
+            + ": "
+            + converter.convert_sae_to_dialect(example)
+        )
